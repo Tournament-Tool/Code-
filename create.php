e668516ec3bb3cf0914e93f66950bb4dc301b19f
B0mbataBG <95975984+B0mbataBG@users.noreply.github.com>
2022-04-05 12:45:29 +0100

    Adding image row to represent brackets

--- create.php
+++ create.php
@@ -39,7 +39,7 @@ else
     die("Error checking for existing table: " . mysqli_error($connection));
 }
 
-$sql = "CREATE TABLE tournaments (id INT NOT NULL AUTO_INCREMENT, title VARCHAR(16), start_date DATE, game VARCHAR(10), creator_name VARCHAR(10), format VARCHAR(10), PRIMARY KEY(id))";
+$sql = "CREATE TABLE tournaments (id INT NOT NULL AUTO_INCREMENT, title VARCHAR(16), start_date DATE, game VARCHAR(10), creator_name VARCHAR(10), format VARCHAR(10), bracket_image VARCHAR(100), PRIMARY KEY(id))";
 
 if (mysqli_query($connection, $sql))
 {
@@ -52,10 +52,10 @@ else
 
 
 
-$title[] = 'Tournament1'; $start_date[] ='2022-04-14'; $game[] = 'League Of Legends'; $creator_name[] = 'user1'; $format[] = 'SWISS';
-$title[] = 'Tournament2'; $start_date[] ='2021-04-14'; $game[] = 'Counter Strike'; $creator_name[] = 'user2'; $format[] = 'Round Robin';
-$title[] = 'Tournament3'; $start_date[] ='2022-06-21'; $game[] = 'Rocket League'; $creator_name[] = 'user3'; $format[] = 'Double Elimination Brackets';
-$title[] = 'HungerGames'; $start_date[] ='2022-09-23'; $game[] = 'Overwatch'; $creator_name[] = 'user1'; $format[] = 'Single Elimination Brackets';
+$title[] = 'Tournament1'; $start_date[] ='2022-04-14'; $game[] = 'League Of Legends'; $creator_name[] = 'user1'; $format[] = 'SWISS'; $images[] = 'LINK OF IMAGE';
+$title[] = 'Tournament2'; $start_date[] ='2021-04-14'; $game[] = 'Counter Strike'; $creator_name[] = 'user2'; $format[] = 'Round Robin'; $images[] = '';
+$title[] = 'Tournament3'; $start_date[] ='2022-06-21'; $game[] = 'Rocket League'; $creator_name[] = 'user3'; $format[] = 'Double Elimination Brackets'; $images[] = '';
+$title[] = 'HungerGames'; $start_date[] ='2022-09-23'; $game[] = 'Overwatch'; $creator_name[] = 'user1'; $format[] = 'Single Elimination Brackets'; $images[] = '';
 
 
 
@@ -63,7 +63,7 @@ $title[] = 'HungerGames'; $start_date[] ='2022-09-23'; $game[] = 'Overwatch'; $c
 // loop through the arrays above and add rows to the table:
 for ($i=0; $i<count($title); $i++)
 {
-    $sql = "INSERT INTO tournaments (title, start_date, game, creator_name, format) VALUES ('$title[$i]', '$start_date[$i]', '$game[$i]', '$creator_name[$i]', '$format[$i]')";
+    $sql = "INSERT INTO tournaments (title, start_date, game, creator_name, format, bracket_image) VALUES ('$title[$i]', '$start_date[$i]', '$game[$i]', '$creator_name[$i]', '$format[$i]', '$images[$i]')";
     print_r($sql);
 
     // no data returned, we just test for true(success)/false(failure):
