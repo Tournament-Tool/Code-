79d4735765fe88652f84f0ad6cddfb572a322778
Mohammad076 <102302375+Mohammad076@users.noreply.github.com>
2022-04-14 16:02:54 +0100

    Update allTeams.php

--- allTeams.php
+++ allTeams.php
@@ -1,6 +1,8 @@
 <?php
 include 'header.php';
 include 'credentials.php';
+
+if (isset($_SESSION['loggedIn'])) {
     //store the username of current logged in user
     $username = $_SESSION['username'];
     
@@ -59,12 +61,61 @@ _END;
                     }
                     
                     else { 
-                        echo "<h2>Failed to join team.</h2>";
+                        echo "<h2>Failed to join team or request has already been sent!</h2>";
                     }
 
     }
 
     mysqli_close($connection);
+}
+
+else {
+
+    $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+    if (!$connection)
+    {
+        die("Connection failed: " . $mysqli_connect_error);
+    }
+
+    $query = "SELECT teams.id , teams.name, users.username, teams.creation_date FROM teams INNER JOIN users ON teams.creator_id = users.id";
+
+    $result = mysqli_query($connection, $query);
+
+    $n = mysqli_num_rows($result);
+
+    if ($n > 0){
+        
+        
+        echo "<body>";
+            echo "<table class=\"allTeams_table\">";
+            for($i = 0; $i < $n; $i++){
+                $row = mysqli_fetch_assoc($result);
+
+                $team_ID = $row['id'];
+                $team_name = $row['name'];
+                $creator_name = $row['username'];
+                $creation_date = $row['creation_date'];
+
+               echo <<<_END
+                 "<tr><td>$team_ID</td><td>$team_name</td><td>$creator_name</td><td>$creation_date</td><td><form method="POST" action="allTeams.php"><button class="btn btn-secondary" id="table_btn" type=submit name="join_team" value="$team_ID">Join</button></form></td></tr>"
+_END;
+            }
+            echo "</table>";
+        echo "</body>";
+        echo "</html>";
+
+    }
+
+    //user mot logged in cannot join a team
+    if (isset($_POST['join_team'])) {
+
+        echo "<h2>You must be signed in to join a team</h2>";
+
+    }
+
+    mysqli_close($connection);
+    
+}
 
 include_once 'footer.php';
 ?>
