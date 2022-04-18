89d50cc70848f1009e53d0a37c24c9af2d0ff8eb
Mohammad076 <102302375+Mohammad076@users.noreply.github.com>
2022-04-18 20:22:59 +0100

    Update tournament.php

--- tournament.php
+++ tournament.php
@@ -2,23 +2,24 @@
 include 'header.php';
 include 'credentials.php';
 
-if (isset($_SESSION['loggedIn'])){
-        $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+if (isset($_GET['id'])) {
+
+    $id = $_GET['id'];
+
+    $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
     if (!$connection)
     {
         die("Connection failed: " . $mysqli_connect_error);
     }
 
-
     $query = "SELECT tournaments.title AS ttitle, creation_date, games.title, format, bracket_image, users.username FROM tournaments INNER JOIN games ON tournaments.id = games.id
-    INNER JOIN users ON tournaments.creator_id = users.id";
+    INNER JOIN users ON tournaments.creator_id = users.id WHERE tournaments.id = '$id'";
 
     $result = mysqli_query($connection, $query);
 
     $n = mysqli_num_rows($result);
 
     if ($n > 0){
-        for($i = 0; $i < $n; $i++){
         $row = mysqli_fetch_assoc($result);
         $title = $row['ttitle'];
         $start_date = $row['creation_date'];
@@ -28,27 +29,27 @@ if (isset($_SESSION['loggedIn'])){
         $bracket_image = $row['bracket_image'];
         echo <<<_END
         <body>
-        <table>
-            <tr><td>{$title}</td></tr>
-            <tr><td>Game: {$game}</td></tr>  <tr><td>Format: {$format}</td></tr>  <tr><td>by: {$creator_name}</td></tr>
-            <tr><td>{$start_date}</td></tr>
-            <tr><td><img src="{$bracket_image}" alt="{$title}" title="{$title}" width="200" height="150"></td></tr>
-            <tr><td><button id="copyButton">Share Tournament</button></td></tr>
+            <h1>{$title}</h1>
+            <h3>Game: {$game} &emsp; Format: {$format} &emsp; by: {$creator_name}</h3>
+            <h4>{$start_date}</h4>
+            <img src="{$bracket_image}" alt="{$title}" title="{$title}" width="200" height="150">
+            <button id="copyButton">Share Tournament</button>
 
         </body>
     </html>
 _END;
     }
-}
 
     else {
         echo "<h1>Tournament not found!</h1>";
     }
-mysqli_close($connection);
+
+    mysqli_close($connection);   
 }
-elseif (!isset($_SESSION['loggedIn'])){
-    echo"You need to be signed in to view this page";
+else {
+    echo "No data received!";
 }
+
 include_once 'footer.php';
 
 ?>
