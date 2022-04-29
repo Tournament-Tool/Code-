0c2e5b605650458fe1f122aa6e9a199355ace5a0
Mohammad076 <102302375+Mohammad076@users.noreply.github.com>
2022-04-29 03:07:24 +0100

    Update signout.php

--- signout.php
+++ signout.php
@@ -4,7 +4,8 @@ if (isset($_SESSION['loggedIn'])){
     unset($_SESSION['loggedIn']);
     header("Refresh:0");
 }elseif (!isset($_SESSION['loggedIn'])){
-    echo "You have logged out";
+    echo "<br><br><br><h2 class=\"text-center\">You have logged out <div class=\"spinner-border\" role=\"status\"><span class=\"sr-only\"></span></div></h2>";
+    header("Refresh:2; URL=about.php");
     }
 include 'footer.php';
 ?>
