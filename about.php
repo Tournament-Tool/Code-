2d9c2cfee500fa1038806ae985661b1e3bbd50dc
Mohammad076 <102302375+Mohammad076@users.noreply.github.com>
2022-04-29 15:56:38 +0100

    Update about.php

--- about.php
+++ about.php
@@ -2,13 +2,16 @@
 
 include 'header.php';
 echo <<< _END
-
-<h2 class="aboutTitle">Tournament Project</h2>
-<div class="aboutInfo">
-    <p> Welcome to our tournament project tool. This wesbite has been created for the purpose of completing the Professional
-    Development unit at Manchester Metropolitan University. This website is for education purposes mainly, and may be improved in the future.
-    For now, enjoy the features that the site has to offer.</p><br>
-    <p class="thanksMsg"> Thanks For Visiting! </p>
+<div class="container">
+    <div class="col-10 col-sm-10 col-md-5 col-lg-7 col-xl-8 col-xxl-10 mx-auto"><br><br><br><br><br>
+        <h2 class="aboutTitle text-center">Tournament Project</h2><br>
+        <div class="aboutInfo">
+            <p> Welcome to our tournament project tool. This website has been created for the purpose of completing the Professional
+            Development unit at Manchester Metropolitan University. This website is for education purposes mainly, and may be improved in the future.
+            For now, enjoy the features that the site has to offer.</p><br>
+            <p class="thanksMsg"> Thanks For Visiting! </p>
+        </div>
+    </div>
 </div>
 _END;
 ?>
