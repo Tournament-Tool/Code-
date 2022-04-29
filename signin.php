a1e08a75d0096b52c7b1ea0455b8ea0194d9caf7
Mohammad076 <102302375+Mohammad076@users.noreply.github.com>
2022-04-29 03:07:07 +0100

    Update signin.php

--- signin.php
+++ signin.php
@@ -7,13 +7,19 @@ if (!$connection)
     die("Connection failed: " . $mysqli_connect_error);
 }
 echo<<<_START
-<form action="signIn.php" method="post">
-Please enter a username and password<br>
-Username: <input type="text" name="username" id="username">
-<br>
-Password: <input type="text" name="password" id="password">
-<input type="submit" value="Submit" name="submit">
-</form>
+<br><br><br><br>
+<div class="container">
+    <div class="col-10 col-sm-10 col-md-8 col-lg-6 col-xl-5 col-xxl-4 mx-auto">
+        <form action="signIn.php" method="post">
+            Please enter a username and password<br><br>
+            Username: <input type="text" name="username" id="username">
+            <br><br>
+            Password: <input type="text" name="password" id="password">
+            <br><br>
+                <input id="login_signup_button" type="submit" value="Submit" name="submit" class="w-25">
+        </form>
+    </div>
+</div>
 _START;
 
     if(isset($_POST['username'])){
@@ -49,8 +55,8 @@ _START;
 
         $getId = mysqli_fetch_assoc($result);
 
-        echo "Hi, $username, you have successfully logged in<br>";
-        header("Refresh:0");
+        echo "<br><h2 class=\"text-center\"> Hi, $username, you have successfully logged in <div class=\"spinner-border\" role=\"status\"><span class=\"sr-only\"></span></div></h2><br>";
+        header("Refresh:2; URL=about.php");
 
     }
 }
