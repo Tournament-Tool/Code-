55c38582a3de6106996643c8ab66da2c71cb80b7
B0mbataBG <95975984+B0mbataBG@users.noreply.github.com>
2022-05-03 07:12:30 +0100

    Add files via upload

--- createTournament.php
+++ createTournament.php
@@ -1,69 +1,121 @@
 <?php
 include 'header.php';
 include 'credentials.php';
-$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-if (!$connection)
-{
-    die("Connection failed: " . $mysqli_connect_error);
-}
+
+
+$message = '';
+
 echo<<<START
     <form action="createTournament.php" method="post">
   Title: <input type="text" name="title" id="tite">
   <br>
-    <Label>Tournament type</label>
-  <select name="format_options" id="format_options">
-    <option>Select tournament type </option>
+  <Label>Tournament format</label>
+    <select name="format_options" id="format_options" placeholder="Select tournament type">
     <option value"single_elimination">Signle Elimination</option>
     <option value"double_elimination">Double Elimination</option>
     <option value"round_robin">Round Robin</option>
   </select>
   <br>
-  Image: <input type="image" name="image" id="image">
-  <br>
   Number of teams: <input type="number_of_teams" name="number_of_teams" id="number_of_teams">
   <br>
-  players_in_team: <input type="players_in_team" name="players_in_team" id="players_in_team">
+  Players in team: <input type="players in_team" name="players_in_team" id="players_in_team">
   <br>
-  prize<input type="prize_pool" name="prize_pool" id="prize_pool"><br>
+  Prize fund:<input type="prize_pool" name="prize_pool" id="prize_pool">
+  <br>
+  Game:<input type="game" name="game" id="game"><br>
   <input type="submit" value="Submit" name="submit">
 </form>
 START;
 
-if(isset($_POST['user'])){
-$id = $_POST['id'];
-$title = $_POST['title'];
-$creation_date = $_POST['creation_date'];
-$format = $_POST['format'];
-$bracket_image = $_POST['bracket_image'];
-$number_of_teams = $_POST['number_of_teams'];
-$players_in_team = $_POST['players_in_team'];
-$prize_pool = $_POST['prize_pool'];
-$game_id = $_POST['game_id'];
-$organisation_id = $_POST['organisation_id'];
-$creator_id = $_POST['creator_id'];
 
-$x = ;
-while ($x <= 32) {
-    echo "The number is: $x <br>";
-    $x +2;
+$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+if (!$connection){
+    die("Connection failed: " . $mysqli_connect_error);
 }
 
+if (isset($_SESSION['loggedIn'])) {
 
-$query = "INSERT INTO `tournaments` (`id`, `title`, `creation_date`, `format`, `bracket_image`, `number_of_teams`, `players_in_team`, `prize_pool`, `game_id`, `organisation_id`, `creator_id`)
-VALUE(`$id`, `$title`, `$creation_date`, `$format`, `$bracket_image`, `$number_of_teams`, `$players_in_team`, `$prize_pool`, `$game_id`, `$organisation_id`, `$creator_id`)";
+    $username = $_SESSION['username'];
+    $getUIdQuery = "SELECT id FROM users WHERE username='$username'"; 
+    $result = mysqli_query($connection, $getUIdQuery);
+    $row = mysqli_fetch_assoc($result);
+    $creator_id = $row['id'];
 
 
-$results = mysqli_query($connection, $query);
+    if(isset($_POST['title'])){
 
-$num = mysqli_num_rows($results);
-if ($num == 0)
-{
-    $q  = "INSERT INTO users(username, password, firstname, lastname, email, date_of_birth) VALUES('$user','$pass','$firstName','$lastName','$email','$dob')";
-mysqli_query($connection, $q);
-    echo "Sign up successful";
+        $title = $_POST['title'];
+        $creation_date = date("Y-m-d");
+        $format = $_POST['format_options'];
+        $number_of_teams = $_POST['number_of_teams'];
+        $players_in_team = $_POST['players_in_team'];
+        $prize_pool = $_POST['prize_pool'];
+        $game = $_POST['game'];
 
-}elseif ($num > 0){
-    echo "Tournament already exists";
+        //$bracket_image = $_POST['bracket_image'];
+        //$organisation_id = $_POST['organisation_id'];
+  
+        $checkQuery = "SELECT * FROM tournaments WHERE title ='$title'";
+        $checkResult = mysqli_query($connection, $checkQuery);
+        $n = mysqli_num_rows($checkResult);
+
+        if ($n > 0){
+            $message = "There is already a tournament with the same name</a>";
+        }
+
+        else{
+            
+            $getGameIdQuery = "SELECT id FROM games WHERE title LIKE '$game'";
+            $resultGameId = mysqli_query($connection, $getGameIdQuery);
+            $rowGameID = mysqli_fetch_assoc($resultGameId);
+            if($rowGameID){
+
+                $game_id = $rowGameID['id'];
+
+                $createTournamentQuery = "INSERT INTO tournaments (title, creation_date, format, number_of_teams, players_in_team, prize_pool, game_id, creator_id) 
+                VALUES ('$title', '$creation_date', '$format', '4', '$players_in_team', '$prize_pool', '$game_id', '$creator_id')";
+                $resultCreateTournament = mysqli_query($connection, $createTournamentQuery);
+
+                if($resultCreateTournament){
+
+                    $getTournamentIdQuery = "SELECT id FROM tournaments WHERE title='$title'";
+                    $resultTournamentId = mysqli_query($connection, $getTournamentIdQuery);
+                    $rowTournamentID = mysqli_fetch_assoc($resultTournamentId);
+                    $tournament_id = $rowTournamentID['id'];
+
+                    for ($x = 0; $x <= 2; $x++) {
+                        echo "The number is: $x <br>";
+                        $createMatchQuery = "INSERT INTO matches (id, tournament_id) VALUES ('$x', '$tournament_id')";
+                        $resultCreateMatch = mysqli_query($connection, $createMatchQuery);
+
+                        if ($resultCreateMatch){
+                            echo "Success";
+                        }
+                        else{
+                            echo "OPS";
+                        }
+                      }
+
+                }
+
+                else{
+                    $message ="Something went wrong with creating the tournament";
+                }
+            }
+
+            else{
+                $message = "The game you have selected cannot be found. <br>";
+            }
+        }
+    }
 }
+else{
+    $message = "You are not logged in.<br>";
 }
+
+echo "$message";
+
+
+
+
 ?>
